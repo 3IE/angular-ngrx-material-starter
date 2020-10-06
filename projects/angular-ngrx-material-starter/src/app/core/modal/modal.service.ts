6bd080c432950865a1e0949a4876dd3f4ac75535
Kévin Bonnot <bonnot.kevin@gmail.com>
2020-10-06 10:01:10 +0200

    Move modal service

--- projects/angular-ngrx-material-starter/src/app/core/modal/modal.service.ts
+++ projects/angular-ngrx-material-starter/src/app/core/modal/modal.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor() { }
+  private ngbModal: NgbModal;
+
+    constructor(
+        ngbModal: NgbModal
+    ) {
+        this.ngbModal = ngbModal;
+    }
+
+    public open(content, options = {}) {
+        return this.ngbModal.open(content, { backdrop: 'static', centered: true, ...options });
+    }
 }
