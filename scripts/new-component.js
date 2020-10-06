e6b7ff95301763ccaf5bda7d02f41d3df7c77c93
Kévin Bonnot <bonnot.kevin@gmail.com>
2020-10-06 10:55:08 +0200

    Script new component

---
+++ scripts/new-component.js
@@ -0,0 +1,12 @@
+const { exec } = require('child_process');
+
+if (process.argv.length !== 3) {
+  console.log('1 single argument must be provided');
+  process.exit(1);
+}
+
+exec(`ng generate component components/${process.argv[2]}`, res => {
+  if (res) {
+    console.log(res.message);
+  }
+});
