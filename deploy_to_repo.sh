fa30fd851743a24b0d5e37075da524af45892ccb
3ieCedricSchneider <cedric.schneider@3ie.fr>
2021-01-07 10:58:01 +0100

    replace azure registry by digitalocean registry

--- deploy_to_repo.sh
+++ deploy_to_repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker tag angular_ngrx_material_starter 3ieregistry.azurecr.io/angular_ngrx_material_starter:${TRAVIS_BRANCH/\//_}_${TRAVIS_BUILD_NUMBER}
-docker push 3ieregistry.azurecr.io/angular_ngrx_material_starter
+docker tag angular_ngrx_material_starter registry.digitalocean.com/registry3ie/angular_ngrx_material_starter:${TRAVIS_BRANCH/\//_}_${TRAVIS_BUILD_NUMBER}
+docker push registry.digitalocean.com/registry3ie/angular_ngrx_material_starter
